164f2ea668dadf19c3d7291c64fbbefb1938fb9a
Daniel Mendoza <Daniel.mendoza.ss@solidaridad.mx>
2026-05-19 11:24:50 -0500

    feature/actualizacion_login

--- src/app/Pages/Login.jsx
+++ src/app/Pages/Login.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { Box, Card, CardContent, TextField, Button, Typography } from "@mui/material";
+import { Box, Card, CardContent, TextField, Button, Typography, Divider } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { FcAcceptDatabase } from "react-icons/fc";
+import "../../App.css" 
+
 
 const LoginModal = () => {
   const navigate = useNavigate(); // Hook para navegación
@@ -10,11 +12,12 @@ const LoginModal = () => {
   navigate("/dashboard"); // ruta del Dashboard
 };
 
-  const handleRegister = () => {
-    navigate("/register"); // Redirige al screen de registro
+  const handleRecuperarpass = () => {
+    navigate("/recuperarpass"); // Redirige al screen de registro
   };
 
   return (
+    
     <Box
       sx={{
         position: "fixed",
@@ -26,64 +29,112 @@ const LoginModal = () => {
         alignItems: "center",
         justifyContent: "center",
         overflow: "hidden",
-        backgroundColor: "#0f1f2e",
+        backgroundColor: "var(--color-secondary)",
         flexDirection: "column",
       }}
     >
-      <FcAcceptDatabase size={120} />
+  
       {/* Card centrado */}
       <Card
         sx={{
-          width: 400,
+          width: 450,
+          height: 480,
           maxWidth: "90%",
           borderRadius: 3,
           boxShadow: 8,
           zIndex: 2,
           backdropFilter: "blur(8px)",
-          backgroundColor: "rgba(255,255,255,0.1)",
+          backgroundColor: "var(--color-primary)",
         }}
       >
-        <CardContent sx={{ p: 4 }}>
-          <Typography variant="h5" textAlign="center" fontWeight="bold" gutterBottom color="white">
-            Task Board
+        <CardContent sx={{ p: 6 }}>
+
+
+       
+        {/*  
+          <Box sx={{ display: "flex", justifyContent: "center"}}>
+            <img
+              src={TaskboardIsotipoOscuro}
+              alt="logo"
+              style={{width: "200px", height: "auto"}}
+            />
+          </Box>
+          */}
+
+
+          
+          <Typography variant="h4" textAlign="center" fontWeight="bold" gutterBottom color="white">
+            INICIO DE SESION
           </Typography>
 
-          <Typography variant="body2" textAlign="center" color="rgba(255,255,255,0.7)" mb={3}>
-            Inicia sesión para continuar
+          <Typography variant="body2" textAlign="center" color="var(--color-blanco)" mb={3}>
+            Accede a tus proyectos
           </Typography>
 
-          <Box component="form" sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+          <Divider sx={{ backgroundColor:"var(--color-blanco)", my: 3 }}/>
+
+          <Typography variant="body2" textAlign="light" color="var(--color-terciario)" mb={1}>
+            CORREO ELECTRONICO
+          </Typography>
+
+          <Box component="form" sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
             <TextField
-              label="Correo electrónico"
+              label="usuario@empresa.com"
               type="email"
               fullWidth
               InputLabelProps={{ style: { color: "white" } }}
-              sx={{ input: { color: "white" }, fieldset: { borderColor: "rgba(255,255,255,0.5)" } }}
-            />
+              sx={{
+              "& .MuiInputBase-input": {
+                color: "white"
+              },
+              "& .MuiOutlinedInput-root": {
+                backgroundColor: "var(--color-secondary)",
+                boxShadow: 8,
+                "& fieldset": {
+                  borderColor: "rgba(255,255,255,0.5)"
+      }
+    }
+  }}
+/>
+
+            <Typography variant="body2" textAlign="light" color="var(--color-terciario)">
+            CONTRASEÑA
+          </Typography>
+
             <TextField
-              label="Contraseña"
-              type="password"
+              label="* * * * * * * * * * * *"
+              type="password" 
               fullWidth
               InputLabelProps={{ style: { color: "white" } }}
-              sx={{ input: { color: "white" }, fieldset: { borderColor: "rgba(255,255,255,0.5)" } }}
-            />
+              sx={{
+              "& .MuiInputBase-input": {
+                color: "white"
+              },
+              "& .MuiOutlinedInput-root": {
+                backgroundColor: "var(--color-secondary)",
+                boxShadow: 8,
+                "& fieldset": {
+                  borderColor: "rgba(255,255,255,0.5)"
+      }
+    }
+  }}
+/>
 
             <Button
               variant="contained"
               size="large"
-              sx={{ mt: 2, backgroundColor: "#00BCD4", "&:hover": { backgroundColor: "#00ACC1" } }}
+              sx={{ mt: 2, backgroundColor: "var(--color-selection)", "&:hover": { backgroundColor: "#00ACC1" } }}
               onClick={handleLogin}
             >
-              Ingresar
-            </Button>
+              Iniciar sesion
+            </Button> 
           </Box>
 
-          {/* Mensaje de registro */}
-          <Box sx={{ textAlign: "center", mt: 3 }}>
+          {/* Mensaje de recuperacion de contraseña */}
+          <Box sx={{ textAlign: "center", mt: 1 }}>
             <Typography variant="body2" color="rgba(255,255,255,0.7)">
-              ¿No tienes cuenta?{" "}
-              <Button variant="text" onClick={handleRegister} sx={{ color: "#00BCD4" }}>
-                Regístrate
+              <Button variant="text" onClick={handleRecuperarpass} sx={{ color: "var(--color-blanco)" }}>
+                ¿Olvidaste tu contraseña?
               </Button>
             </Typography>
           </Box>
