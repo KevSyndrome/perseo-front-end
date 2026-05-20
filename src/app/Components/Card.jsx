26ab5b650b32127648373a2726dc8fee05c027e1
Daniel Mendoza <Daniel.mendoza.ss@solidaridad.mx>
2026-05-20 00:01:43 -0500

    feature_dashboard_cards

--- src/app/Components/Card.jsx
+++ src/app/Components/Card.jsx
@@ -2,7 +2,8 @@ export default function Card({ icon, label, color, onClick }) {
   return (
     <button
       onClick={onClick}
-      className={`flex w-full min-h-[160px] flex-col items-center justify-center gap-3 rounded-xl p-8 text-white transition hover:brightness-90 cursor-pointer ${color}`}
+      style={{ backgroundColor: color }}  
+      className="flex w-full min-h-[160px] flex-col items-center justify-center gap-3 rounded-xl p-8 text-white transition hover:brightness-90 cursor-pointer"
     >
       {icon}
       <span className="text-base font-medium">{label}</span>
--- src/app/Pages/Dashboard.jsx
+++ src/app/Pages/Dashboard.jsx
@@ -13,24 +13,25 @@ const Dashboard = () => {
   return (
     <div className="flex h-full w-full flex-col">
       <Breadcrumb />
-      <h1 className="text-2xl font-bold text-slate-800">Bienvenido usuario</h1>
+      <h1 style={{color: "var(--color-primary)"}}className="text-2xl font-bold text-slate-800">Bienvenido usuario</h1>
 
       <div  className="flex flex-1 flex-col gap-8 pt-6">
         <div className="grid grid-cols-3 gap-4">
           <Card
             icon={<BarChart3 size={36} />}
+            
             label="Proyectos activos"
-            color="bg-primary hover:bg-primary-hover"
+            color="var(--color-selection)"  
           />
           <Card
             icon={<Link2 size={36} />}
             label="Unirse al proyecto"
-            color="bg-secondary hover:bg-secondary-hover"
+            color="var(--color-selection)"
           />
           <Card
             icon={<Plus size={36} />}
             label="Crear proyecto"
-            color="bg-tertiary hover:bg-tertiary-hover"
+            color="var(--color-grey)"
           />
         </div>
 
